dd2d63c94b9b9bed09512b3ae7af0af7a60a2302
Christophe Boutter <boutta@gmail.com>
2018-02-11 19:35:42 +0100

    Add CORS Filter for local angular dev site

--- src/main/java/sbv/turnier/TurnierController.java
+++ src/main/java/sbv/turnier/TurnierController.java
@@ -2,13 +2,16 @@ package sbv.turnier;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class TurnierController {
 
@@ -21,6 +24,10 @@ public class TurnierController {
         logger.debug("GET turnier called");
         return Arrays.asList(
                 new Turnier().setId(counter.getAndIncrement()).setName("Test1"),
-                new Turnier().setId(counter.getAndIncrement()).setName("Test2"));
+                new Turnier().setId(counter.getAndIncrement()).setName("Test2").setEndDate(LocalDate.of(2018, 02, 12)),
+                new Turnier().setId(counter.getAndIncrement()).setName("Test3").setEndDate(LocalDate.of(2018, 02, 13)),
+                new Turnier().setId(counter.getAndIncrement()).setName("Test4").setEndDate(LocalDate.of(2018, 02, 14)),
+                new Turnier().setId(counter.getAndIncrement()).setName("Test5"),
+                new Turnier().setId(counter.getAndIncrement()).setName("Test6"));
     }
 }
