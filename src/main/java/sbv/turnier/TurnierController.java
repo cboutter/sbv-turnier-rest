1bc3e9124d98132b925bb0604954efe88c337b9e
Christophe Boutter <boutta@gmail.com>
2018-02-11 14:52:11 +0100

    Enable log4j logging

--- src/main/java/sbv/turnier/TurnierController.java
+++ src/main/java/sbv/turnier/TurnierController.java
@@ -1,5 +1,7 @@
 package sbv.turnier;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,10 +12,13 @@ import java.util.concurrent.atomic.AtomicLong;
 @RestController
 public class TurnierController {
 
+    private static final Logger logger = LogManager.getLogger(TurnierController.class);
+
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/turnier")
     public List<Turnier> turnier() {
+        logger.debug("GET turnier called");
         return Arrays.asList(
                 new Turnier().setId(counter.getAndIncrement()).setName("Test1"),
                 new Turnier().setId(counter.getAndIncrement()).setName("Test2"));
