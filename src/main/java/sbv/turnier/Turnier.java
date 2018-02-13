827e1ec2bbc92ba7d039bd72121479104f9e959c
Christophe Boutter <boutta@gmail.com>
2018-02-13 10:32:03 +0100

    Change id to Long so it is nullable

--- src/main/java/sbv/turnier/Turnier.java
+++ src/main/java/sbv/turnier/Turnier.java
@@ -4,13 +4,13 @@ import java.time.LocalDate;
 
 public class Turnier {
 
-    private long id;
+    private Long id;
     private String name;
     private LocalDate startDate;
     private LocalDate endDate;
     private LocalDate registerEndDate;
 
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
